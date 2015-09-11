b56035a832e3234c2a1b0aaffd770f5217bfa105
Ross McDonald <mcdonaldrossc@gmail.com>
2015-09-11 12:17:37 -0500

    Playing around with maud and requests.

--- src/handlers/api.rs
+++ src/handlers/api.rs
@@ -12,19 +12,28 @@ pub fn api(req: &mut Request) -> IronResult<Response> {
     match req.method {
         Get => {
             // Collect object ID (or set it to empty string if not found)
-            let obj_id = req.extensions.get::<Router>().unwrap().find("obj_id").unwrap_or("");
+            let obj_id = req.extensions.get::<Router>().unwrap().find("obj_id").unwrap_or("n/a");
             
             let user_agent = req.headers.get::<UserAgent>().unwrap();
             // TODO Perform some simple vetting of user-agent to discern browser v programmatic/API access
             // let content_type = "text/plain".parse::<Mime>().unwrap();
             let content_type = "text/html".parse::<Mime>().unwrap();
+
+            let remote_addr = req.remote_addr.to_string();
+            let user_ip = remote_addr.split(":").nth(0).unwrap();
             
             // let response_body: String = format!("Hello!\n\nObject ID: \'{}\'", obj_id);
             let mut response_body: String = String::new();
             html!(response_body, {
                 html {
-                    h1 "Welcome!"
-                    p { "You have reached API endpoint for object: " $obj_id }
+                    head {
+                        title "API"
+                    }
+                    body {
+                        h1 "Welcome!"
+                        p { "You have reached the API endpoint for object: " b { $obj_id } }
+                        p { "Your IP address is: " b { $user_ip } }
+                    }
                 }
             });
             Ok(Response::with((content_type, status::Ok, response_body)))
